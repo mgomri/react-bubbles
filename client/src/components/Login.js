67de7289d897760d533327141e620bb4e7103fa5
mgomri <mel77gomri@gmail.com>
2020-04-24 11:59:20 -0400

    added a login and an authorization header

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import NavLinks from './NavLinks';
+
+
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  const initialState = {
+    username: '',
+    password: ''
+  }
+
+  const history = useHistory();
+
+  const [credentials, setCredentials] = useState(initialState);
+
+  const onChangeHandler = e => {
+    e.preventDefault();
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  };
+
+  const login = e => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', credentials)
+    .then(res => {
+      localStorage.setItem('token', JSON.stringify(res.data.payload));
+      history.push('/colors');
+    })
+    .catch(err => console.log(err));
+  }
   return (
-    <>
+    <div className='login-form'>
+      <NavLinks />
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <h3>Login to see our bubbles</h3>
+      <form className='form' onSubmit={login}>
+        <label htmlFor='username'>Username</label>
+        <input
+          type='text'
+          name='username'
+          value={credentials.username}
+          onChange={onChangeHandler}
+        />
+         <label htmlFor='password'>Password</label>
+        <input
+          type='password'
+          name='password'
+          value={credentials.password}
+          onChange={onChangeHandler}
+        />
+        <button type='submit' className='button'>Login</button>
+      </form>
+    </div>
   );
 };
 
