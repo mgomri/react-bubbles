a1112c7e3b97d8108cda3a9bdb77f9711d15b102
mgomri <mel77gomri@gmail.com>
2020-04-24 12:32:59 -0400

    Private route added

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
-import NavLinks from './NavLinks';
+import NavBar from './NavBar';
 
 
 
